644672b52f9dafc643a6b6ac0c8286581bde7a77
Alex Ross <alxrg@me.com>
2023-04-17 17:03:21 -0400

    updated working list with characteristics

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, FlatList, ScrollView, View, SafeAreaView, TextInput, Button} from 'react-native';
+import { dogs, cats } from './breed'
+import { useState } from 'react';
+import Item from './Item';
 
 export default function App() {
+  const [search, setSearch] = useState('');
+  const [species, setSpecies] = useState('dogs');
+
+  const toggleSpecies = (newSpecies) => {
+    setSpecies(newSpecies);
+  };
+
+  let data = species === 'dogs' ? dogs : cats;
+  let emojiSpecies = species === 'dogs' ? 'dogs 🐶🐺🐕‍🦺🐩🐕' : 'cats 🐱🐈‍⬛🐈😺🦁';
+  const filteredData = data.filter
+  (item => item.breed.toLowerCase().includes(search.toLowerCase()));
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <SafeAreaView style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button title="🐶🐺🐕‍🦺🐩🐕" onPress={() => toggleSpecies('dogs')} />
+        <Text style={styles.orText}> Or </Text>
+        <Button title="🐱🐈‍⬛🐈😺🦁" onPress={() => toggleSpecies('cats')} />
+      </View>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder='Search'
+          onChangeText={setSearch}
+          value={search}
+        />
+        {search.length > 0 && (
+          <TouchableOpacity onPress={() => setSearch('')}>
+            <Text style={styles.cancelButton}>X</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+
+      <Text>{emojiSpecies ? `Below is a list of ${emojiSpecies}` : 'Choose a species'}</Text>
+
+      <FlatList style={styles.flatlist}
+        data={filteredData}
+        renderItem={({ item }) =>
+             <Item title={`${item.breed}`} data={item} />
+        }
+        keyExtractor={item => `${item.breed}+${item.index}` }
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +61,53 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  separator: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#ccc',
+    marginTop: 5,
+  },
+  flatlist: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    flex: 1,
+    width: '100%',
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 10,
+    height: 30,
+  },
+  cancelButton: {
+    marginLeft: 5,
+    padding: 5,
+    borderRadius: 5,
+    backgroundColor: 'black',
+    color: 'white',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginVertical: 10,
+    paddingVertical: 5,
+  },
+  orText: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
   },
 });
---
+++ Item.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+export default function Item({ data, species }) {
+  const { breed } = data;
+  const characteristics = Object.keys(data).filter((key) => key !== "breed");
+
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{breed}</Text>
+      <FlatList
+        data={characteristics}
+        renderItem={({ item, index }) => (
+          <View
+            style={[
+              styles.propertyContainer,
+              { backgroundColor: index % 2 === 0 ? "#D3D3D3" : "#fff" },
+            ]}
+            key={item}
+          >
+            <Text style={styles.propertyName}>{item}</Text>
+            <View style={styles.propertyValue}>
+              {[...Array(data[item])].map((_, i) => (
+                <Text key={i} style={styles.star}>
+                  ★
+                </Text>
+              ))}
+            </View>
+          </View>
+        )}
+        keyExtractor={(item) => item}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    marginVertical: 8,
+    marginHorizontal: 4,
+  },
+  title: {
+    fontSize: 32,
+  },
+  propertyContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+    paddingHorizontal: 20,
+  },
+  propertyName: {
+    fontSize: 13,
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  propertyValue: {
+    flexShrink: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  star: {
+    paddingTop: 3,
+    fontSize: 8,
+    marginRight: 5,
+  },
+});
